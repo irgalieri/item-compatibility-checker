678107982d538c8d985a9e129487f8fa6dd558a2
Ignacio Rodrigo Galieri <ignacio.galieri@nubicall.com>
2021-02-06 18:11:11 -0500

    init app

---
+++ app.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type App struct {
+	Router *mux.Router
+}
+
+func (a *App) Initialize() {
+	a.Router = mux.NewRouter()
+	a.initializeRoutes()
+}
+
+func (a *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(addr, a.Router))
+}
+
+func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/ping", a.pingChecker).Methods("GET")
+}
+
+func (a *App) pingChecker(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("pong"))
+}
